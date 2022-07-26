ca49c619df47ce7aa22be8109fbacd7ab4e481bf
abudukelimu.aipida <abudukelimu.aipida@digitalreasoning.com>
2022-07-26 19:43:57 -0400

    edited script

--- create_manifest.sh
+++ create_manifest.sh
@@ -2,7 +2,7 @@
 folders=( $(find gold-set -type d -maxdepth 1) )
 
 for (( i=1; i<${#folders[@]}; i++ )); do
-    find ${folders[i]}/chat -type f | sed "s/.*\///" | grep -v manifest.txt > ${folders[i]}/chat/manifest.txt
-    find ${folders[i]}/email -type f | sed "s/.*\///" | grep -v manifest.txt > ${folders[i]}/email/manifest.txt
+    find ${folders[i]}/chat -type f | grep -v manifest.txt | sed -e 's/^/gs:\/\/csurv-input-dr-csurv50-qa\//' > ${folders[i]}/chat/manifest.txt
+    find ${folders[i]}/email -type f | grep -v manifest.txt | sed -e 's/^/gs:\/\/csurv-input-dr-csurv50-qa\//' > ${folders[i]}/email/manifest.txt
 done
 

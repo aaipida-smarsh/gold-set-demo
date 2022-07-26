44371d9f3b3a20a3fde161f2a46784006b474fbd
abudukelimu.aipida <abudukelimu.aipida@digitalreasoning.com>
2022-07-26 17:08:03 -0400

    Added script for manifest

--- create_manifest.sh
+++ create_manifest.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+folders=( $(find gold-set -type d -maxdepth 1) )
+
+for (( i=1; i<${#folders[@]}; i++ )); do
+    find ${folders[i]}/chat -type f | sed "s/.*\///" | grep -v manifest.txt > ${folders[i]}/chat/manifest.txt
+    find ${folders[i]}/email -type f | sed "s/.*\///" | grep -v manifest.txt > ${folders[i]}/email/manifest.txt
+done
 

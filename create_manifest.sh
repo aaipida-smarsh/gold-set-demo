5b1aa4f62cba063f25c2e7c080909f95759ca838
abudukelimu.aipida <abudukelimu.aipida@digitalreasoning.com>
2022-07-26 20:05:52 -0400

    script change

--- create_manifest.sh
+++ create_manifest.sh
@@ -5,4 +5,3 @@ for (( i=1; i<${#folders[@]}; i++ )); do
     find ${folders[i]}/chat -type f | grep -v manifest.txt | sed -e 's/^/gs:\/\/csurv-input-dr-csurv50-qa\//' > ${folders[i]}/chat/manifest.txt
     find ${folders[i]}/email -type f | grep -v manifest.txt | sed -e 's/^/gs:\/\/csurv-input-dr-csurv50-qa\//' > ${folders[i]}/email/manifest.txt
 done
-

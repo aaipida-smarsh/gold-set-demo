4b02645a7de216197d534dc0de68b8407c4f7d42
aaipida-smarsh <105655225+aaipida-smarsh@users.noreply.github.com>
2022-11-18 13:12:50 -0500

    Update create_manifest.sh

--- create_manifest.sh
+++ create_manifest.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-folders=( $(find gold-set -type d -maxdepth 1) )
-
-for (( i=1; i<${#folders[@]}; i++ )); do
-    find ${folders[i]}/chat -type f | grep -v manifest.txt | sed -e 's/^/gs:\/\/csurv-input-dr-csurv50-qa\//' > ${folders[i]}/chat/manifest.txt
-    find ${folders[i]}/email -type f | grep -v manifest.txt | sed -e 's/^/gs:\/\/csurv-input-dr-csurv50-qa\//' > ${folders[i]}/email/manifest.txt
-done
+echo "Hello World"
